dcc66c0218fda744c1460ec278953234fd8735f7
Oğuzhan <akcahanoguz@gmail.com>
2021-10-18 15:47:28 +0300

    partials created, active navigation edited.

--- app.js
+++ app.js
@@ -2,9 +2,22 @@ const express = require("express"); // express module added.
 
 const app = express(); // express func started.
 
+//Template Engine
+app.set("view engine", "ejs"); //View engine setted as "ejs" module.
+
+//Middlewares
+app.use(express.static("public"));
+
 //Routing
 app.get("/", (req, res) => {
-  res.send("INDEX SAYFASI");
+  res.status(200).render("index", {
+    page_name: "index",
+  });
+});
+app.get("/about", (req, res) => {
+  res.status(200).render("about", {
+    page_name: "about",
+  });
 });
 
 const port = 3000; //Port
