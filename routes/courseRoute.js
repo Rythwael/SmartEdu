5c52c729017e1d1b9b63f7a19c26d63f7933adc8
Oğuzhan <akcahanoguz@gmail.com>
2021-10-20 12:28:16 +0300

    Slugify added.

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.route('/').post(courseController.createCourse);
 router.route('/').get(courseController.getAllCourses);
-router.route('/:id').get(courseController.getCourse);
+router.route('/:slug').get(courseController.getCourse);
 
 
 module.exports = router;
