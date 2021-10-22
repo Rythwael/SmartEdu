84c87b8d09eaa1c4224e74d0db9eecb05dc563f1
Oğuzhan <akcahanoguz@gmail.com>
2021-10-22 17:55:32 +0300

    dashboard updated.

--- controllers/authController.js
+++ controllers/authController.js
@@ -28,7 +28,7 @@ exports.loginUser = (req, res) => {
           if(same){ //if passwords are same
             // USER SESSION
             req.session.userID = user._id;
-            res.status(200).redirect('/');
+            res.status(200).redirect('/users/dashboard');
           }
         })
       }
@@ -49,8 +49,10 @@ exports.logOutUser = (req,res) => {
 };
 
 // Dashboard Page
-exports.getDashboardPage = (req, res) => {
+exports.getDashboardPage = async (req, res) => {
+  const user = await User.findOne({_id: req.session.userID});
   res.status(200).render("dashboard", {
     page_name: "dashboard",
+    user
   });
 };
